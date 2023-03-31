40854576547209e1cc33ba19b370ffb02b81a565
Aleksandr Ivanov <solo12zw74@gmail.com>
2023-03-30 21:17:01 -0600

    Implement server, based on the example file
    Here I also was need to calculate the root tree hash
    to keep it once on the server

--- server/index.js
+++ server/index.js
@@ -8,15 +8,16 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT = 'ddd59a2ffccddd60ff47993312821cd57cf30f7f14fb82937ebe2c4dc78375aa';
+const concat = (left, right) => keccak256(Buffer.concat([left, right]));
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
   const body = req.body;
 
   // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
+  const isInTheList = verifyProof(body.proof, body.name, MERKLE_ROOT);
+  if (isInTheList) {
     res.send("You got a toy robot!");
   }
   else {
